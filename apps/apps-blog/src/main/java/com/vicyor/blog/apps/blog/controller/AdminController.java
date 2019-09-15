a061427da6815c81097b6a28d41217a2f620e3d0
vicyor <2457569580@qq.com>
2019-09-16 00:39:49 +0800

    项目更新

--- apps/apps-blog/src/main/java/com/vicyor/blog/apps/blog/controller/AdminController.java
+++ apps/apps-blog/src/main/java/com/vicyor/blog/apps/blog/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.vicyor.blog.apps.blog.controller;
 
+import com.vicyor.blog.apps.blog.repository.CommentRepository;
 import com.vicyor.blog.apps.blog.repository.EsBlogRepository;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -22,11 +23,15 @@ public class AdminController {
     ElasticsearchTemplate elasticsearchTemplate;
     @Autowired
     EsBlogRepository repository;
+    @Autowired
+    CommentRepository commentRepository;
 
     @RequestMapping("/delete")
     public String deleteAll() {
         repository.deleteAll();
+        commentRepository.deleteAll();
         elasticsearchTemplate.deleteIndex("blog");
+        elasticsearchTemplate.deleteIndex("comment");
         return "删除成功";
     }
 
@@ -54,15 +59,11 @@ public class AdminController {
                 .endObject()
                 .startObject("cdate")
                 .field("type", "date")
-                .field("fielddata", true)
-                .field("format", "custom")
-                .field("pattern", "yyyy-MM-dd hh:mm:ss")
+                .field("format", "yyyy-MM-dd HH:mm:ss")
                 .endObject()
                 .startObject("udate")
                 .field("type", "date")
-                .field("fielddata", true)
-                .field("format", "custom")
-                .field("pattern", "yyyy-MM-dd hh:mm:ss")
+                .field("format", "yyyy-MM-dd HH:mm:ss")
                 .endObject()
                 .startObject("count")
                 .field("type", "long")
@@ -77,6 +78,50 @@ public class AdminController {
                 .endObject();
 
         elasticsearchTemplate.putMapping("blog", "blog", builder);
+        return "mapping成功";
+    }
+
+    @RequestMapping("/create/blog")
+    public String index() {
+        elasticsearchTemplate.createIndex("blog");
+        return "创建index成功";
+    }
+
+    @RequestMapping("/create/comment")
+    public String indexComment() {
+        elasticsearchTemplate.createIndex("comment");
+        return "创建index成功";
+    }
+
+    @RequestMapping("/mapping/comment")
+    public String mappingComment() throws Exception {
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+                .startObject()
+                .startObject("properties")
+                .startObject("id")
+                .field("type", "text")
+                .endObject()
+                .startObject("content")
+                .field("type", "text")
+                .field("analyzer", "ik_max_word")
+                .endObject()
+                .startObject("username")
+                .field("type", "keyword")
+                .endObject()
+                .startObject("cdate")
+                .field("type", "date")
+                .field("format", "yyyy-MM-dd hh:mm:ss")
+                .endObject()
+                .startObject("blogId")
+                .field("type", "text")
+                .endObject()
+                .startObject("image")
+                .field("type", "text")
+                .endObject()
+                .endObject()
+                .endObject();
+
+        elasticsearchTemplate.putMapping("comment", "comment", builder);
         return "mapping成功";
     }
 }
