afb2f8a60f02a2e4e3642c36dad5d288cd0ba54e
vicyor <2457569580@qq.com>
2019-10-04 08:45:48 +0800

    修改日志切面逻辑

--- apps/apps-blog/src/main/java/com/vicyor/blog/apps/blog/log/LogAspectJ.java
+++ apps/apps-blog/src/main/java/com/vicyor/blog/apps/blog/log/LogAspectJ.java
@@ -9,8 +9,12 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * 作者:姚克威
@@ -27,16 +31,48 @@ public class LogAspectJ {
 
     @Around("pointCut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
         LogAnnotation annotation = method.getAnnotation(LogAnnotation.class);
         String content = annotation.value();
-        if (content == null || content.trim().length() == 0)    {
+        if (content == null || content.trim().length() == 0) {
             content = method.getName();
         }
-        log.info(String.format("%s  开始", content));
+        log.info(String.format("%s  开始", this.new Log(url, ip, classMethod, args, content)));
         Object proceed = joinPoint.proceed();
-        log.info(String.format("%s  结束", content));
         return proceed;
+    }
+
+    private class Log {
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
+        private String content;
+
+        public Log(String url, String ip, String classMethod, Object[] args, String content) {
+            this.url = url;
+            this.ip = ip;
+            this.classMethod = classMethod;
+            this.args = args;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return "Log{" +
+                    "url='" + url + '\'' +
+                    ", ip='" + ip + '\'' +
+                    ", classMethod='" + classMethod + '\'' +
+                    ", args=" + Arrays.toString(args) +
+                    ", content='" + content + '\'' +
+                    '}';
+        }
     }
 }
