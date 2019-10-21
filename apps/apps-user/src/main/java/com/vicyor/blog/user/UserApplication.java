36c9560009077f8862ce13524300837fa4b3ce1c
vicyor <2457569580@qq.com>
2019-10-21 22:54:24 +0800

    添加个人模块,未完善

---
+++ apps/apps-user/src/main/java/com/vicyor/blog/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.vicyor.blog.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 作者:姚克威
+ * 时间:2019/10/20 17:45
+ **/
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
+}
