ed5c916c9d99f3d45607ec288b9b4df83c56d02e
vicyor <2457569580@qq.com>
2019-10-24 15:51:23 +0800

    个人模块开发

--- apps/apps-user/src/main/java/com/vicyor/blog/apps/UserApplication.java
+++ apps/apps-user/src/main/java/com/vicyor/blog/apps/UserApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * 作者:姚克威
  * 时间:2019/10/20 17:45
  **/
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @MapperScan("com.vicyor.blog.apps.mapper")
 @ServletComponentScan("com.vicyor.blog.apps.filter")
 public class UserApplication {
