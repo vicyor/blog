fa768b8130e747833336ddeef5266e94eb857674
vicyor <2457569580@qq.com>
2019-10-25 00:56:51 +0800

    修改url为服务器url

--- apps/apps-user/src/main/java/com/vicyor/blog/apps/config/security/WebSecurityConfig.java
+++ apps/apps-user/src/main/java/com/vicyor/blog/apps/config/security/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**/login/**").permitAll()
+                .antMatchers("/**/login/**","/**/auth/**","/**/oauth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
